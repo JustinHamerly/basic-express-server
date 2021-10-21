1439844e0c9a3421d49ea3ce7038a3f063655d5b
Justin Hamerly <suvimountain@icloud.com>
2021-10-20 17:34:41 -0700

    fixed test and get send from lab 02

--- src/server.js
+++ src/server.js
@@ -1,26 +1,30 @@
 'use strict';
 
 const express = require('express');
-const dotenv = require('dotenv').config();
+// const dotenv = require('dotenv').config();
+// const PORT = process.env.PORT || 3001;
 const logger = require('./middleware/logger');
 const validator = require('./middleware/validator');
 const error404 = require('./error-handlers/404');
 const error500 = require('./error-handlers/500');
 const app = express();
-const PORT = process.env.PORT || 3001;
 
 app.use(logger);
 app.use(express.json());
 
 app.get('/person', validator, (request, response) => {
-  let name = request.query.name;
+  console.log(response);
+  let name = request.query;
   response.send(name);
 });
 
-app.use(error500);
 app.use(error404);
+app.use(error500);
 
 module.exports = {
   app,
-  start: app.listen(3001, () => console.log(`Server Running on port ${PORT}`)),
+  // start: app.listen(3001, () => console.log(`Server Running on port ${PORT}`)),
+  start: (PORT) => {
+    app.listen(PORT, () => console.log(`Server Running on port ${PORT}`));
+  },
 };
